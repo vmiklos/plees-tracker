ef1a26bba001bdcc93e230dcac0aec99765383d8
Miklos Vajna <vmiklos@vmiklos.hu>
2019-12-15 21:26:26 +0100

    plees-tracker: missing javadoc

--- app/src/main/java/hu/vmiklos/plees_tracker/AppDatabase.java
+++ app/src/main/java/hu/vmiklos/plees_tracker/AppDatabase.java
@@ -3,6 +3,10 @@ package hu.vmiklos.plees_tracker;
 import androidx.room.Database;
 import androidx.room.RoomDatabase;
 
+/**
+ * Contains the database holder and serves as the main access point for the
+ * stored data.
+ */
 @Database(entities = {Sleep.class}, version = 1)
 public abstract class AppDatabase extends RoomDatabase
 {
