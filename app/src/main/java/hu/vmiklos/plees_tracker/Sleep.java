204cf2e966b642e6c4803b39409af47e91aa2d5a
Miklos Vajna <vmiklos@vmiklos.hu>
2019-12-15 21:30:06 +0100

    plees-tracker: missing license header

--- app/src/main/java/hu/vmiklos/plees_tracker/Sleep.java
+++ app/src/main/java/hu/vmiklos/plees_tracker/Sleep.java
@@ -1,3 +1,9 @@
+/*
+ * Copyright 2019 Miklos Vajna. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
 package hu.vmiklos.plees_tracker;
 
 import androidx.room.ColumnInfo;
