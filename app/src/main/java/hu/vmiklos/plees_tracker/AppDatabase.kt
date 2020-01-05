358460bfcf6b534c542f0b91dcb739d8ea2c2336
Miklos Vajna <vmiklos@vmiklos.hu>
2020-01-05 08:26:05 +0100

    Convert AppDatabase to Kotlin

---
+++ app/src/main/java/hu/vmiklos/plees_tracker/AppDatabase.kt
@@ -0,0 +1,19 @@
+/*
+ * Copyright 2019 Miklos Vajna. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+package hu.vmiklos.plees_tracker
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+
+/**
+ * Contains the database holder and serves as the main access point for the
+ * stored data.
+ */
+@Database(entities = [Sleep::class], version = 1, exportSchema = false)
+abstract class AppDatabase : RoomDatabase() {
+    abstract fun sleepDao(): SleepDao
+}
