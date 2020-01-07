0528f5353dc235b4bd1f2967786bd5e4b91ba10f
Miklos Vajna <vmiklos@vmiklos.hu>
2020-01-07 07:45:37 +0100

    Convert MainActivityInstrumentedTest to Kotlin
    Which was the last Java source file.

---
+++ app/src/androidTest/java/hu/vmiklos/plees_tracker/MainActivityInstrumentedTest.kt
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2019 Miklos Vajna. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+package hu.vmiklos.plees_tracker
+
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
+
+/**
+ * Instrumented tests for MainActivity.
+ */
+@RunWith(AndroidJUnit4::class)
+class MainActivityInstrumentedTest {
+    @get:Rule
+    var activityScenarioRule = ActivityScenarioRule(MainActivity::class.java)
+
+    @Test
+    fun testCountStat() {
+        val dataModel = DataModel.dataModel
+        dataModel.deleteSleeps()
+
+        onView(withId(R.id.start_stop)).perform(click())
+        onView(withId(R.id.start_stop)).perform(click())
+        onView(withId(R.id.count_stat)).check(matches(withText("1")))
+    }
+}
