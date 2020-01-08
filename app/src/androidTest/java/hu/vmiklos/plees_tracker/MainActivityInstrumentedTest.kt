3031f49a97e2bac3d61c589802bef038226ad523
Miklos Vajna <vmiklos@vmiklos.hu>
2020-01-08 07:28:05 +0100

    Fit ktlint warnings in MainActivityInstrumentedTest

--- app/src/androidTest/java/hu/vmiklos/plees_tracker/MainActivityInstrumentedTest.kt
+++ app/src/androidTest/java/hu/vmiklos/plees_tracker/MainActivityInstrumentedTest.kt
@@ -6,18 +6,16 @@
 
 package hu.vmiklos.plees_tracker
 
-import androidx.test.ext.junit.rules.ActivityScenarioRule
-import androidx.test.ext.junit.runners.AndroidJUnit4
-
-import org.junit.Rule
-import org.junit.Test
-import org.junit.runner.RunWith
-
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
 
 /**
  * Instrumented tests for MainActivity.
