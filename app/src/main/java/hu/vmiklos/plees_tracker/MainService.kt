64e7900fefb2304fbf4f803ebfeb3253d66e0594
Miklos Vajna <vmiklos@vmiklos.hu>
2020-01-09 09:08:26 +0100

    Remove unused imports in MainService

--- app/src/main/java/hu/vmiklos/plees_tracker/MainService.kt
+++ app/src/main/java/hu/vmiklos/plees_tracker/MainService.kt
@@ -6,15 +6,12 @@
 
 package hu.vmiklos.plees_tracker
 
-import android.app.Notification
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.PendingIntent
 import android.app.Service
 import android.content.Intent
 import android.os.IBinder
-import android.util.Log
-
 import androidx.core.app.NotificationCompat
 
 /**
