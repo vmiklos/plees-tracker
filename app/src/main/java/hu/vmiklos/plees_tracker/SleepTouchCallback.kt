09ca21bf22d9a8bb468ead40a343c3b4d6e7540a
Miklos Vajna <vmiklos@vmiklos.hu>
2020-01-10 08:50:39 +0100

    Remove unused imports in SleepTouchCallback

--- app/src/main/java/hu/vmiklos/plees_tracker/SleepTouchCallback.kt
+++ app/src/main/java/hu/vmiklos/plees_tracker/SleepTouchCallback.kt
@@ -6,27 +6,28 @@
 
 package hu.vmiklos.plees_tracker
 
-import android.util.Log
-import android.view.View
 import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.RecyclerView
-
 import com.google.android.material.snackbar.Snackbar
 
 /**
  * This callback handles deletion of one recorded sleep in the sleep list. Listens to left/right swipes only.
  */
-class SleepTouchCallback(private val mAdapter: SleepsAdapter)
-    : ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT) {
+class SleepTouchCallback(private val mAdapter: SleepsAdapter) :
+    ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT) {
 
-    override fun onMove(recyclerView: RecyclerView,
-               viewHolder: RecyclerView.ViewHolder,
-               target: RecyclerView.ViewHolder): Boolean {
+    override fun onMove(
+        recyclerView: RecyclerView,
+        viewHolder: RecyclerView.ViewHolder,
+        target: RecyclerView.ViewHolder
+    ): Boolean {
         return false
     }
 
-    override fun onSwiped(viewHolder: RecyclerView.ViewHolder,
-                 direction: Int) {
+    override fun onSwiped(
+        viewHolder: RecyclerView.ViewHolder,
+        direction: Int
+    ) {
         val dataModel = DataModel.dataModel
         val sleep = mAdapter.data!!.get(viewHolder.getAdapterPosition())
         dataModel.deleteSleep(sleep)
