8fefb9dd8ddd2ca6574a3e3358852b9eb02f349c
Miklos Vajna <vmiklos@vmiklos.hu>
2020-02-18 08:05:57 +0100

    Readability: move lambda out of parentheses
    Tested-by: Travis

--- app/src/main/java/hu/vmiklos/plees_tracker/SleepTouchCallback.kt
+++ app/src/main/java/hu/vmiklos/plees_tracker/SleepTouchCallback.kt
@@ -37,9 +37,9 @@ class SleepTouchCallback(
         val view = viewHolder.itemView
         val snackbar = Snackbar.make(view, R.string.deleted, Snackbar.LENGTH_LONG)
         val dataModel = DataModel.dataModel
-        snackbar.setAction(dataModel.getString(R.string.undo), {
-                    viewModel.insertSleep(sleep)
-                })
+        snackbar.setAction(dataModel.getString(R.string.undo)) {
+            viewModel.insertSleep(sleep)
+        }
 
         snackbar.show()
     }
