50f05da28b8b22c75a314f44b77593ab9064a668
Miklos Vajna <vmiklos@vmiklos.hu>
2020-04-03 22:57:19 +0200

    Clean up dubious facts and fix sleep edit
    Tested-by: Travis

--- app/src/main/java/hu/vmiklos/plees_tracker/SleepViewModel.kt
+++ app/src/main/java/hu/vmiklos/plees_tracker/SleepViewModel.kt
@@ -13,17 +13,11 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.launch
 import java.util.*
-import kotlin.collections.ArrayList
-
 
 /**
  * This is the view model of SleepActivity, providing coroutine scopes.
  */
 class SleepViewModel : ViewModel() {
-
-    private val sleepFactsList = ArrayList<String>()
-    var rand: Random? = null
-
 
     fun showSleep(activity: SleepActivity, sid: Int) {
         viewModelScope.launch {
@@ -33,35 +27,8 @@ class SleepViewModel : ViewModel() {
             start.text = DataModel.formatTimestamp(Date(sleep.start))
             val stop = activity.findViewById<TextView>(R.id.sleep_stop)
             stop.text = DataModel.formatTimestamp(Date(sleep.stop))
-            val sleepFacts = activity.findViewById<TextView>(R.id.sleep_facts_text)
-
-
-            // init Sleep Facts Array
-            initSleepFactsArray()
-
-            // Generate Random Facts when Opening Sleep Activity
-            val facts: String = randomFacts(sleepFactsList)
-            sleepFacts.text = facts
         }
     }
-
-    private fun randomFacts(sleepFactsList: ArrayList<String>): String {
-
-        rand = Random()
-        return sleepFactsList[rand!!.nextInt(sleepFactsList.size)]
-
-    }
-
-    private fun initSleepFactsArray() {
-        sleepFactsList.add("Longer sleep has been shown to improve many aspects of athletic and physical performance.")
-        sleepFactsList.add("Short sleep duration is associated with an increased risk of weight gain and obesity in both children and adults.")
-        sleepFactsList.add("Poor sleep affects hormones that regulate appetite. Those who get adequate sleep tend to eat fewer calories than those who don’t.")
-        sleepFactsList.add("Good sleep can maximize problem-solving skills and enhance memory. Poor sleep has been shown to impair brain function.")
-        sleepFactsList.add("Sleeping less than 7–8 hours per night is linked to an increased risk of heart disease and stroke.")
-        sleepFactsList.add("Poor sleeping patterns are strongly linked to depression, particularly for those with a sleeping disorder.")
-        sleepFactsList.add("Getting at least 8 hours of sleep can improve your immune function and help fight the common cold.")
-    }
-
 
     fun editSleep(activity: SleepActivity, sid: Int, isStart: Boolean) {
         viewModelScope.launch {
