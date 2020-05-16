20f331cac47c545a42e9ebb3fe898da13ad6207c
Miklos Vajna <vmiklos@vmiklos.hu>
2020-05-16 14:35:00 +0200

    SharedPreferencesChangeListener: replace cascade if with when
    Tested-by: Travis

--- app/src/main/java/hu/vmiklos/plees_tracker/SharedPreferencesChangeListener.kt
+++ app/src/main/java/hu/vmiklos/plees_tracker/SharedPreferencesChangeListener.kt
@@ -18,12 +18,16 @@ class SharedPreferencesChangeListener : SharedPreferences.OnSharedPreferenceChan
     fun applyTheme(sharedPreferences: SharedPreferences) {
         val themeFollowSystem = sharedPreferences.getBoolean("follow_system_theme", true)
         val darkTheme = sharedPreferences.getBoolean("dark_mode", false)
-        if (themeFollowSystem) {
-            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM)
-        } else if (darkTheme) {
-            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
-        } else {
-            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+        when {
+            themeFollowSystem -> {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM)
+            }
+            darkTheme -> {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+            }
+            else -> {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+            }
         }
     }
 }
