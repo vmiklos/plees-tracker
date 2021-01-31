b5ed77a37b451d588498b5d77e4480c47fe911d9
Miklos Vajna <vmiklos@vmiklos.hu>
2021-01-31 15:05:10 +0100

    CI: auto-upload a debug APK for each release
    Change-Id: Ic062674c79e12d7e7a187e5c71360253601b68c2

---
+++ tools/ci-build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -ex
+#
+# Copyright 2021 Miklos Vajna. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+#
+
+#
+# This script runs all the tests for CI purposes.
+#
+
+./gradlew build
+./gradlew test
+./gradlew connectedAndroidTest
+
+mkdir dist
+cp $(find . -name *.apk) dist/
+
+# vim:set shiftwidth=4 softtabstop=4 expandtab:
