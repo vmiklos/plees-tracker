2595bc157b8f0033837059734adff28955385540
Miklos Vajna <vmiklos@vmiklos.hu>
2023-11-01 19:53:35 +0100

    Update dependencies (2023-11)
    Change-Id: Idf60b98d5737ba7a0d5a28e24540a5982fdfca47

--- app/src/main/java/hu/vmiklos/plees_tracker/DataModel.kt
+++ app/src/main/java/hu/vmiklos/plees_tracker/DataModel.kt
@@ -35,14 +35,14 @@ import org.apache.commons.csv.CSVPrinter
 import org.apache.commons.csv.CSVRecord
 
 val MIGRATION_1_2 = object : Migration(1, 2) {
-    override fun migrate(database: SupportSQLiteDatabase) {
-        database.execSQL("ALTER TABLE sleep ADD COLUMN rating INTEGER NOT NULL DEFAULT 0")
+    override fun migrate(db: SupportSQLiteDatabase) {
+        db.execSQL("ALTER TABLE sleep ADD COLUMN rating INTEGER NOT NULL DEFAULT 0")
     }
 }
 
 val MIGRATION_2_3 = object : Migration(2, 3) {
-    override fun migrate(database: SupportSQLiteDatabase) {
-        database.execSQL("ALTER TABLE sleep ADD COLUMN comment TEXT NOT NULL DEFAULT ''")
+    override fun migrate(db: SupportSQLiteDatabase) {
+        db.execSQL("ALTER TABLE sleep ADD COLUMN comment TEXT NOT NULL DEFAULT ''")
     }
 }
 
