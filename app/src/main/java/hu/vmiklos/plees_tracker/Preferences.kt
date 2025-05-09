53d64216a56e939f307316f433c0813817d0f5aa
Miklos Vajna <vmiklos@vmiklos.hu>
2025-05-09 22:40:38 +0200

    bedtime/wakeup: show the value of these in the preferences activity
    Towards eliminating the inconsistency that these reminders can be set
    via a menu item, while everything else is in the preferences activity.
    
    This just shows the preferences so far, no modification is possible yet.

--- app/src/main/java/hu/vmiklos/plees_tracker/Preferences.kt
+++ app/src/main/java/hu/vmiklos/plees_tracker/Preferences.kt
@@ -11,6 +11,11 @@ import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
 
 class Preferences : PreferenceFragmentCompat() {
+    private fun padMinute(raw: String): String {
+        val fro = ":([0-9])$".toRegex()
+        val to = ":0$1"
+        return raw.replace(fro, to)
+    }
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.preferences, rootKey)
         val autoBackupPath = findPreference<Preference>("auto_backup_path")
@@ -18,6 +23,22 @@ class Preferences : PreferenceFragmentCompat() {
             val preferences = DataModel.preferences
             val path = preferences.getString("auto_backup_path", "")
             it.summary = path
+        }
+        val wakeup = findPreference<Preference>("wakeup")
+        wakeup?.let {
+            val preferences = DataModel.preferences
+            val value = preferences.getString("wakeup", "")
+            if (value != null) {
+                it.summary = padMinute(value)
+            }
+        }
+        val bedtime = findPreference<Preference>("bedtime")
+        bedtime?.let {
+            val preferences = DataModel.preferences
+            val value = preferences.getString("bedtime", "")
+            if (value != null) {
+                it.summary = padMinute(value)
+            }
         }
     }
 }
